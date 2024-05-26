11320068b85a51672ef2683893057a484f10fc70
Padmajakandula <157293834+Padmajakandula@users.noreply.github.com>
2024-05-26 11:08:12 +0530

    Add files via upload

---
+++ sum-square-difference.py
@@ -0,0 +1,22 @@
+sum_of_squares=sum(i**2 for i in range(1,101))
+sum_of_numbers=sum(i for i in range(1,101))
+square_of_the_sum=sum_of_numbers**2
+difference=sum_of_squares-square_of_the_sum
+print("the difference of the sum of squares and square of sum of the first hundred is",difference)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
